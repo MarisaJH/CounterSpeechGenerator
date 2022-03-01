b484853cb2841ac91ba903b7ce1fe8eafdb6e007
Lesh Chadick <chajl-24@rhodes.edu>
2022-03-01 16:06:50 -0600

    This is the Tumblr Bot, but it will not work without the key

---
+++ Tumblrbot/keyFunctions.py
@@ -0,0 +1,6 @@
+def createKeys ():
+    keys = ['SV3U9c1ZO6HnSMyKFHQc48WgrmdM4zVNX3bfbWgNQZvF4AABKF',
+    '7veZeJ1bD45zEGmuvSMBnH9WBGI3t27vAAAPy32DaSnZ65ZVAx',
+    'EzISR5n1pkBpC9JUTnorj8WraP7Way28H05kuT7GkcwPfIlKHs',
+    'Snr8fh20a3MRElvMDJvyCKXLup06e8uldvyIjhRLLSOWdyX915']
+    return keys
